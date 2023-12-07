2e19dedfacb9b1143d089c4723b68b1686fe6edb
unknown <pmjochemczyk@gmail.com>
2023-12-07 13:41:25 +0100

    Portfolio 0.1.1

--- frontend/src/pages/MainMenu.js
+++ frontend/src/pages/MainMenu.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const MainMenu = () => {
+  const [isGamesOpen, setIsGamesOpen] = useState(false);
+
+  const handleGamesHover = () => {
+    setIsGamesOpen(true);
+  };
+
+  const handleGamesLeave = () => {
+    const gamesList = document.querySelector(".games-list");
+    const isHoveringGames = gamesList && gamesList.matches(":hover");
+    if (!isHoveringGames) {
+      setIsGamesOpen(false);
+    }
+  };
+
   return (
     <div className="header">
       <div className="logo">
@@ -10,7 +24,17 @@ const MainMenu = () => {
       <div className="menu">
         <Link to="/">Home</Link>
         <Link to="/notes/">Notes</Link>
-        <Link to="/snake/">Snake Game</Link>
+        <div className="games-dropdown"
+        onMouseEnter={handleGamesHover}
+        onMouseLeave={handleGamesLeave}>
+          <Link>Games</Link>
+          {isGamesOpen && (
+            <div className="games-list">
+              <Link to="/snake/">Snake</Link>
+              <Link to="/tictactoe/">Tic Tac Toe</Link>
+            </div>
+          )}
+        </div>
         <Link to="/todo">ToDo</Link>
         <Link to="/contact">Contact</Link>
       </div>
---
+++ frontend/src/pages/TicTacToe/TicTacToeGameMenu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "../../components/TicTacToe/Header";
+
+const TicTacToeGameMenu = () => {
+  return (
+    <div className="container dark">
+      <div className="app">
+        <Header />
+        {/* <Routes>
+
+        </Routes> */}
+      </div>
+    </div>
+  );
+};
+
+export default TicTacToeGameMenu;
